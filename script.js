5e6c34d87d3ff551cb0e89df1957822dd86c6e02
yancarlodev <yancarlodc@gmail.com>
2022-05-13 15:48:51 -0300

    adicionando teste de mesa

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//               2  3  4  5  6  7  8  9  10 11 12
 let numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//               0  1  2  3  4  5  6  7  8  9  10
 
 const input = document.querySelector('input')
 const rollButton = document.querySelector('button')
